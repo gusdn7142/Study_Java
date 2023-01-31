35b85553ef1cb89ada8dcfb30af21f78c78be96c
gusdn7142 <gusdnchl7144@naver.com>
2023-02-01 08:24:19 +0900

    Junit5 Study
    실습1) Assumptions 클래스의 조건 메서드 assumeTrue()와 assumingThat() 테스트
    실습2) Assertions 클래스의 조건 메서드 assertThat() 테스트
    실습3) @Enabled~~, @Disabled~~ 어노테이션 테스트

--- Java_Test/src/test/java/java_test/StudyTest.java
+++ Java_Test/src/test/java/java_test/StudyTest.java
@@ -1,10 +1,14 @@
 package java_test;
 
 
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
+//import org.assertj.core.api.*;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.condition.*;
 
 import java.time.Duration;
-
 
 
 
@@ -13,58 +17,35 @@ class StudyTest {
 
     @Test
     @DisplayName("스터디 만들기 \uD83D\uDE31")
+    @EnabledOnOs({OS.MAC, OS.LINUX, OS.WINDOWS})
+    @EnabledOnJre({JRE.JAVA_8, JRE.JAVA_9, JRE.JAVA_10,JRE.JAVA_11} )
+    @EnabledIfEnvironmentVariable(named = "TEST_ENV", matches= "LOCAL")
+    //@EnabledIfSystemProperty(named = "java.version", matches = "11.0.15")
     void create_new_study(){
 
-        //Study 객체 생성
-        //Study study = new Study(-10);
+        String test_env = System.getenv("TEST_ENV");   //환경변수 TEST_ENV의 값 "LOCAL"을 조회
 
-        //Study 객체 Null 검증
-        //Assertions.assertNotNull(study);
-
-        //Study 객체의 StudyStatus 값이 DRAFT인지 검증
-        //Assertions.assertEquals(StudyStatus.DRAFT, study.getStatus(), new Supplier<String>() {
-        //    @Override
-        //    public String get() {
-        //        return "스터디를 처음 만들면 상태값이 DRAFT여야 한다.";
-        //    }
-        //});
-        //Assertions.assertEquals(StudyStatus.DRAFT, study.getStatus(), "스터디를 처음 만들면" + StudyStatus.DRAFT + " 상태다.");  //테스트가 성공하던 실패하던 무조건 문자열 연산을 한다.
-        //Assertions.assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면" + StudyStatus.DRAFT + " 상태다.");   //테스트가 실패했을때만 messageSupplier 문자열 연산을 하기 때문에 람다식을 쓰면 성능상 유리
+        System.out.println(test_env);
+        Assumptions.assumeTrue("LOCAL".equalsIgnoreCase(test_env));   //test_env의 값이 "LOCAL"인지 확인
 
 
-        //Study 객체의 limit 값이 0보다 큰지 검증
-        //Assertions.assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.");
-
-
-        //모든 테스트에 대한 에러 메시지를 한번에 보는 방법
-        //Assertions.assertAll(
-        //        ()->Assertions.assertNotNull(study),
-        //        ()->Assertions.assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 " + StudyStatus.DRAFT + " 상태다."),
-        //        ()->Assertions.assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
-        //);
-
-
-        //발생한 예외를 검증
-        //IllegalAccessException exception = Assertions.assertThrows(IllegalAccessException.class, () -> new Study(-10));
-        //Assertions.assertEquals("limit은 0보다 커야 한다.", exception.getMessage());   //(실제 예외메시지, 기대하는 예외 메시지)
-
-
-        //특정 시간 (100 millisecond) 안에 테스트가 수행되는지 확인 (안전한 방법)
-        //Assertions.assertTimeout(Duration.ofMillis(100), () -> {
-        //    new Study(10);
-        //    Thread.sleep(300);   //1초 지연
-        //});
-
-
-
-        //특정 시간 (100 millisecond) 안에 테스트가 수행되는지 확인하고 + 특정 시간이 지나면 테스트를 강제로 종료시킴
-        Assertions.assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
-            new Study(10);
-            Thread.sleep(300);   //1초 지연
+        Assumptions.assumingThat("LOCAL".equalsIgnoreCase(test_env),() ->{
+            System.out.println("local");
+            Study actual = new Study(100);
+            //Assertions.assertThat(actual.getLimit()).isGreaterThan(0);
         });
-        // TODO ThreadLocal
 
-        //System.out.println("create");
+
+        Assumptions.assumingThat("keesun".equalsIgnoreCase(test_env),() ->{
+            System.out.println("keesun");
+            Study actual = new Study(10);
+            Assertions.assertThat(actual.getLimit()).isGreaterThan(0);
+
+        });
+
+        Study actual = new Study(10);
+        Assertions.assertThat(actual.getLimit()).isGreaterThan(0);  //isGreaterThan(0) : 0보다 큰 숫자인지 확인
+
     }
 
     @Test
