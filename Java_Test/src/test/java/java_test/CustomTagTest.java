468e64fd58eaf4b49824de144385ae574fd529d8
gusdn7142 <gusdnchl7144@naver.com>
2023-02-03 12:39:02 +0900

    Junit5 Study
    실습) 커스텀 태그 어노테이션 생성 및 사용 : @FastTest, @SlowTest

---
+++ Java_Test/src/test/java/java_test/CustomTagTest.java
@@ -0,0 +1,49 @@
+package java_test;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class CustomTagTest {
+
+    @FastTest
+    @Test
+    @DisplayName("스터디 만들기 fast")
+    void create_new_study(){
+
+        Study actual = new Study(10);
+        Assertions.assertThat(actual.getLimit()).isGreaterThan(0);  //isGreaterThan(0) : 0보다 큰 숫자인지 확인
+        System.out.println("create_new_study");
+    }
+
+    @SlowTest
+    @Test
+    @DisplayName("스터디 만들기 slow")
+    void createNew_study_again(){
+        System.out.println("createNew_study_again");
+    }
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("before each");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("after each");
+    }
+
+
+}
---
+++ Java_Test/src/test/java/java_test/SlowTest.java
@@ -0,0 +1,17 @@
+package java_test;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Target(ElementType.METHOD)           //메서드에 사용 가능
+@Retention(RetentionPolicy.RUNTIME)   //런타임까지 유지
+@Tag("slow")                          //slow라는 태그에서 수행
+@Test                                 //테스트에서 동작
+public @interface SlowTest {
+}
