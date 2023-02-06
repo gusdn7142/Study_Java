9657a804fef82421af7d194cb93a5de305b19cf4
gusdn7142 <gusdnchl7144@naver.com>
2023-02-06 09:59:35 +0900

    Junit5 Study
    실습) 테스트 반복 어노테이션 사용 : @RepeatedTest, @ParameterizedTest

---
+++ Java_Test/src/test/java/java_test/RepeatTest.java
@@ -0,0 +1,46 @@
+package java_test;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+class RepeatTest {
+
+    @DisplayName("스터디 만들기")
+    @RepeatedTest(value = 10, name = ".{displayName}, {currentRepetition}/{totalRepetitions}")
+    void repeatTest(RepetitionInfo repetitionInfo){
+        System.out.println("Test Case Info : " + repetitionInfo + " , 반복 횟수 : " + repetitionInfo.getTotalRepetitions());
+    }
+
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요"})
+    void ParameterizedTest(String message){
+        System.out.println(message);
+    }
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("before each");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("after each");
+    }
+
+
+}
