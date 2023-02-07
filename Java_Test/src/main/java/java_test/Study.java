e2778ea53b698fb2be783c68cb13786cc4cc5f0a
gusdn7142 <gusdnchl7144@naver.com>
2023-02-07 10:22:49 +0900

    Junit5 Study
    실습1) 인자 개수만큼 테스트 반복 : @ParameterizedTest, @ValueSource, @ConvertWith 사용
    실습2) 인자값 조합 수 만큼 테스트 반복 : @ParameterizedTest, @CsvSource, @AggregateWith 사용

--- Java_Test/src/main/java/java_test/Study.java
+++ Java_Test/src/main/java/java_test/Study.java
@@ -4,12 +4,23 @@ public class Study {
 
     private StudyStatus status = StudyStatus.DRAFT;
     private int limit;
+    private String name;
+
+    public Study(int limit, String name) {
+        this.limit = limit;
+        this.name = name;
+    }
 
     public Study(int limit) {   //throws IllegalAccessException{
 //        if(limit<0){
 //            throw new IllegalAccessException("limit은 0보다 커야 한다.");
 //        }
         this.limit = limit;
+    }
+
+
+    public String getName() {
+        return name;
     }
 
     public StudyStatus getStatus(){
@@ -20,5 +31,14 @@ public class Study {
         return limit;
     }
 
+
+    @Override
+    public String toString() {
+        return "Study{" +
+                "status=" + status +
+                ", limit=" + limit +
+                ", name='" + name + '\'' +
+                '}';
+    }
 
 }
---
+++ Java_Test/src/test/java/java_test/RepeatTest2.java
@@ -0,0 +1,80 @@
+package java_test;
+
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.*;
+
+
+class RepeatTest2 {
+
+    @DisplayName("테스트 반복1")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    //@ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요"})
+    @ValueSource(ints = {10, 20, 40})
+    //@NullAndEmptySource       //@NullSource + @EmptySource
+    void ParameterizedTest(@ConvertWith(StudyConverter.class) Study study){      //string message
+        //System.out.println(message);
+        System.out.println(study.getLimit());
+    }
+
+
+    @DisplayName("테스트 반복2")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+    void ParameterizedTest2(@AggregateWith(StudyAggregator.class) Study study){                      //1. Integer limit, String name...   2. ArgumentsAccessor argumentsAccessor
+        //Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));   //limit, name
+        System.out.println(study);
+    }
+
+
+    static class StudyConverter extends SimpleArgumentConverter{
+
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            Assertions.assertEquals(Study.class, targetType, "Can only convert to Study");
+            return new Study(Integer.parseInt(source.toString()));
+        }
+    }
+
+    static class StudyAggregator implements ArgumentsAggregator {
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext context) throws ArgumentsAggregationException {
+            return new Study(accessor.getInteger(0), accessor.getString(1));    //limit, name
+        }
+    }
+
+
+
+
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("before each");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("after each");
+    }
+
+
+}
