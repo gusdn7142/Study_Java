f1fe0544530dd6d7c38390775c36fa514d2d7969
gusdn7142 <gusdnchl7144@naver.com>
2023-02-14 10:00:53 +0900

    Junit5 Study
    실습) Junit4 기본 어노테이션 테스트

---
+++ Java_Test/src/test/java/java_test/StudyJUnit4Test.java
@@ -0,0 +1,35 @@
+package java_test;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/*
+ * JUunit 5 플랫폼에 junit-vintage-engine 의존성을 추가하여 Junit 4 사용
+ */
+public class StudyJUnit4Test {
+
+
+    @Before   //junit.Before 사용
+    public void before(){
+        System.out.println("before");
+    }
+
+    @After   //junit.After 사용
+    public void After(){
+        System.out.println("After");
+    }
+
+
+    @Test   //junit.Test 사용
+    public void createTest1(){
+        System.out.println("test1");
+    }
+
+    @Test  //junit.Test 사용
+    public void createTest2(){
+        System.out.println("test2");
+    }
+
+}
